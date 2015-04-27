1b229c0901db0338bc4e86e75716a6d2f7bba2b8
Ozan Turgut <ozanturgut@gmail.com>
2015-04-26 17:50:05 -0700

    :sunrise:

---
+++ scripts/download.js
@@ -0,0 +1,12 @@
+var crawl = require('../lib/crawl');
+var fs = require('fs');
+var path = require('path');
+
+var recordsUrl = 'http://www.cde.ca.gov/ta/ac/ap/apidatafiles.asp';
+var destinationDir =  path.join(__dirname, '..', 'data');
+
+if (!fs.existsSync(destinationDir)) {
+	fs.mkdirSync(destinationDir);
+}
+
+crawl(recordsUrl , destinationDir);
